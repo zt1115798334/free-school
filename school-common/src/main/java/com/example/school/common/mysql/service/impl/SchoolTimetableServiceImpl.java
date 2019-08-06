c8bab9ab2813daffb1719fa9cdd7a2137c691f97
zhangtong9498@qq.com <fallinlovewithff>
2019-08-06 21:14:06 +0800

    调整代码

--- school-common/src/main/java/com/example/school/common/mysql/service/impl/SchoolTimetableServiceImpl.java
+++ school-common/src/main/java/com/example/school/common/mysql/service/impl/SchoolTimetableServiceImpl.java
@@ -11,11 +11,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.WeekFields;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.*;
@@ -41,13 +40,23 @@ public class SchoolTimetableServiceImpl implements SchoolTimetableService {
     public JSONObject findSchoolTimetable(Long userId, String semester, Integer weeklyTimes) {
         SchoolAdministration schoolAdministration = schoolAdministrationService.findSchoolAdministration(userId);
         List<SchoolTimetable> timetableList = schoolTimetableRepository.findByStudentIdAndSemesterAndWeeklyTimes(schoolAdministration.getStudentId(), semester, weeklyTimes);
-        Map<Short, Map<String, SchoolTimetable>> timetableMap = timetableList.stream()
-                .collect(groupingBy(SchoolTimetable::getClassTimes, toMap(SchoolTimetable::getWeek, Function.identity())));
-        List<List<String>> curriculumList = SysConst.getAllClassTimesCode().stream()
-                .map(classTimes -> {
-                    Map<String, SchoolTimetable> weekMap = timetableMap.getOrDefault(classTimes, Collections.emptyMap());
-                    return SysConst.getAllWeekType().stream()
-                            .map(week -> Optional.ofNullable(weekMap.get(week))
+//        Map<Short, Map<String, SchoolTimetable>> timetableMap = timetableList.stream()
+//                .collect(groupingBy(SchoolTimetable::getClassTimes, toMap(SchoolTimetable::getWeek, Function.identity())));
+//        List<List<String>> curriculumList = SysConst.getAllClassTimesCode().stream()
+//                .map(classTimes -> {
+//                    Map<String, SchoolTimetable> weekMap = timetableMap.getOrDefault(classTimes, Collections.emptyMap());
+//                    return SysConst.getAllWeekType().stream()
+//                            .map(week -> Optional.ofNullable(weekMap.get(week))
+//                                    .map(SchoolTimetable::getCurriculum)
+//                                    .orElse("-")).collect(toList());
+//                }).collect(toList());
+        Map<String, Map<Short, SchoolTimetable>> timetableMap = timetableList.stream()
+                .collect(groupingBy(SchoolTimetable::getWeek, toMap(SchoolTimetable::getClassTimes, Function.identity())));
+        List<List<String>> curriculumList = SysConst.getAllWeekType().stream()
+                .map(week -> {
+                    Map<Short, SchoolTimetable> weekMap = timetableMap.getOrDefault(week, Collections.emptyMap());
+                    return SysConst.getAllClassTimesCode().stream()
+                            .map(classTimes -> Optional.ofNullable(weekMap.get(classTimes))
                                     .map(SchoolTimetable::getCurriculum)
                                     .orElse("-")).collect(toList());
                 }).collect(toList());
